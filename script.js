09b88b0a18e58be7cc46818785560250e2e43c04
dthill <dthill@gmx.net>
2018-08-08 01:41:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function initMap() {
+  var userLocation = {lat: 40.8054491, lng: -73.9654415}
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: userLocation,
+    zoom: 14
+  });
+  var marker = new google.maps.Marker({
+      position: userLocation,
+      map: map,
+      title: "User Location"
+    });
+}
